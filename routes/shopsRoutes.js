19f1e190067344220810f48be5dc3d1229934feb
Race Carpenter <racecarpenter@gmail.com>
2017-06-29 11:27:23 -0700

    updated id page

--- routes/shopsRoutes.js
+++ routes/shopsRoutes.js
@@ -19,31 +19,32 @@ router.get('/:id', function(req, res) {
   })
 })
 
-router.get('/edit/:id', function(req, res) {
-  knex.raw(`select * from shops where id=${req.params.id}`).then(function(shops) {
-    res.send(shops.rows);
-  })
-})
-
-router.get('/new', function(req, res) {
-  knex.raw(`select * from shops`).then(function(shops) {
-    res.send(shops.rows);
-  })
-})
-
-router.put('/:id', function(req, res) {
-  knex.raw(`update shops set name='${req.body.name}', city='${req.body.city}' where id=${req.params.id}`).then(function() {
+router.post('/edit/:id', function(req, res) {
+  knex.raw(`update shops set name = '${req.body.name}', city = '${req.body.city}' where id=${req.params.id}`).then(function() {
     knex.raw(`select * from shops`).then(function(shops) {
-      res.send(shops.rows);
+      res.render('shops', {
+        shops: shops.rows
+      })
     })
   })
 })
 
-router.post('/', function(req, res) {
+
+router.post('/remove/:id', function(req, res) {
+  knex.raw(`delete from shops where id=${req.params.id}`).then(function() {
+    knex.raw(`select * from shops`).then(function(shops) {
+      res.render('shops', {
+        shops: shops.rows
+      })
+    })
+  })
+})
+
+router.post('/add', function(req, res) {
   knex.raw(`insert into shops(name, city) values('${req.body.name}', '${req.body.city}')`).then(function() {
     knex.raw(`select * from shops`).then(function(shops) {
-      res.render('shopsNEW', {
-
+      res.render('shops', {
+        shops: shops.rows
       });
     })
   })
